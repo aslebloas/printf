afb9c6a1e9985cd5c0e3cdb3649ef13e236f39e9
aslebloas <aslebloas@gmail.com>
2018-07-29 20:12:07 +0000

    Updated header file in test directory

--- test/holberton.h
+++ test/holberton.h
@@ -1,6 +1,13 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
+/**
+ * struct type - struct type
+ * @type: format type
+ * @f: function to print the type
+ */
+
 int _printf(const char *format, ...);
+int _putchar(char c);
 
 #endif /* _HOLBERTON_H */
