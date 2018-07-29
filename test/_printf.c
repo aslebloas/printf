949f84cd8fec4c99b1822311a3d01b5a8725df07
JackWanaCode <kazucuong@yahoo.com>
2018-07-29 20:24:40 +0000

    Structure for _printf

---
+++ test/_printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _printf - format and print data in standard output
+ * @format: constant string argument
+ *
+ * Return: number of char printed or -1 if fails.
+
+int _printf(const char *format, ...)
+{
+return (0);
+}
