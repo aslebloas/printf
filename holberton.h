0cc3ad65d8b3d811702b4caa0e499fce707cd62e
JackWanaCode <kazucuong@yahoo.com>
2018-07-30 21:46:06 +0000

    add comment for struct define

--- holberton.h
+++ holberton.h
@@ -8,6 +8,13 @@ int print_per(void);
 int print_char(va_list ap);
 int print_int(va_list ap);
 
+/**
+ * struct _printf_ - Store data of format and function pointer
+ * @format: format will be printed
+ * @f: function pointer
+ *
+ * Description: No
+ */
 typedef struct _printf_
 {
 	char format;
