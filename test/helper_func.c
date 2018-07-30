83cd49e4cc5716326a942b39034ffaf128b714d6
JackWanaCode <kazucuong@yahoo.com>
2018-07-30 15:33:38 +0000

    update with using array of struct

--- test/helper_func.c
+++ test/helper_func.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <stdarg.h>
 #include "holberton.h"
 
 /**
@@ -16,18 +17,79 @@ int _putchar(char c)
 
 /**
  * print_str - prints a string
- * @str: string argument
+ * @ap: string argument
  *
- * Return: an int
+ * Return: number of character(s) are printed
  */
-int print_str(char *str)
+int print_str(va_list ap)
 {
 	int i = 0;
+	char *str;
 
+	str = va_arg(ap, char *);
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
 		i++;
 	}
 	return (i);
+}
+
+/**
+ * print_per - prints a percentage sign
+ * @ap: percentage
+ *
+ * Return: number of character(s) are printed
+ */
+int print_per(void)
+{
+	_putchar('%');
+	return (1);
+}
+
+/**
+ * print_char - prints a character
+ * @ap: character
+ *
+ * Return: number of character(s) are printed
+ */
+int print_char(va_list ap)
+{
+	char c = va_arg(ap, int);
+
+	_putchar(c);
+	return (1);
+}
+
+/**
+ * print_num - prints a number
+ * @ap: characters of numbers
+ *
+ * Return: number of character(s) are printed
+ */
+int print_int(va_list ap)
+{
+	int max_digit_int = 1000000000;
+	int temp_num = 0;
+	int num = va_arg(ap, int);
+	int count = 0;
+
+	while (num / max_digit_int == 0)
+		max_digit_int /= 10;
+	while (max_digit_int > 0)
+	{
+		temp_num = num / max_digit_int;
+		num = num % max_digit_int;
+		if (temp_num < 0)
+		{
+			_putchar('-');
+			count++;
+			temp_num = -temp_num;
+			num = -num;
+		}
+		_putchar(temp_num + '0');
+		count++;
+		max_digit_int /= 10;
+	}
+	return (count);
 }
--- test/main.c
+++ test/main.c
@@ -15,19 +15,18 @@ int main(void)
 	// void *addr;
 
 	/* Test for char, string and % */
-	len = _printf("Let's try to printf a simple sentence.\n");
-	len2 = printf("Let's try to printf a simple sentence.\n");
+	_printf("Let's try to printf a simple sentence.\n");
+	printf("Let's try to printf a simple sentence.\n");
         _printf("Character:[%c]\n", 'H');
         printf("Character:[%c]\n", 'H');
-        _printf("String:[%s]\n", "I am a string !");
-        printf("String:[%s]\n", "I am a string !");
-	len = _printf("Percent:[%%]\n");
-	len2 = printf("Percent:[%%]\n");
-
+        len = _printf("String:[%s]\n", "I am a string !");
+        len2 = printf("String:[%s]\n", "I am a string !");
+	_printf("Percent:[%%]\n");
+	printf("Percent:[%%]\n");
+	_printf("Length:[%d, %i]\n", len, len);
+        printf("Length:[%d, %i]\n", len2, len2);
 /*	ui = (unsigned int)INT_MAX + 1024;
 	addr = (void *)0x7ffe637541f0;
-	_printf("Length:[%d, %i]\n", len, len);
-	printf("Length:[%d, %i]\n", len2, len2);
 	_printf("Negative:[%d]\n", -762534);
 	printf("Negative:[%d]\n", -762534);
 	_printf("Unsigned:[%u]\n", ui);
