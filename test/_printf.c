3d7531f2f7d6ed320c0d53096224966ac9862121
JackWanaCode <kazucuong@yahoo.com>
2018-07-31 18:34:37 +0000

    edit c file

--- test/_printf.c
+++ test/_printf.c
@@ -12,19 +12,27 @@
 
 int _printf(const char *format, ...)
 {
+	/* Variables declaration */
 	int i = 0, j = 0;
-	int count = 0;
-	p array[5] = {
-		{'s', print_str},
-		{'c', print_char},
-		{'%', print_per},
-		{'i', print_int},
-		{'d', print_int}
+	int count = 0, countf = 0;
+	p array[] = {
+		{"s", print_str},
+		{"c", print_char},
+		{"%", print_per},
+		{"i", print_int},
+		{"d", print_int},
+		{"u", print_unsigned_int};
+		{"o", print_octal_num};
+		{"X", print_HEX_num};
+		{"x", print_hex_num};
+		{NULL, NULL}
 	};
 	va_list ap;
 
+	/* Return NULL if format is null */
 	if (format == NULL)
 		return (-1);
+
 	va_start(ap, format);
 	while (format[i] != '\0')
 	{
@@ -35,17 +43,28 @@ int _printf(const char *format, ...)
 		}
 		else
 		{
+			/* Loop through array to check if format[i + 1] exists */
 			j = 0;
-			while (j < 5)
+			countf = 0;
+			while (j < 10)
 			{
-				if (format[i + 1] == array[j].format)
+				if (array[j].format == NULL)
+					return (-1);
+				if (!format[i + 1])
+					return (-1);
+				if (format[i + 1] == *(array[j].format))
 				{
-					count += array[j].f(ap);
+					/* Call the func corresponding to char */
+					countf = array[j].f(ap);
+					count += countf;
 					i++;
-					j = 5;
+					j = 10;
 				}
 				j++;
 			}
+			/* if format[i + 1] is not in array, print it */
+			/* if (countf == 0) */
+			/*return (-1);*/
 		}
 		i++;
 	}
