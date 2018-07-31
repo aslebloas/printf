79b5eff185cb7ae08a5bfce0d68899c5a29063b3
JackWanaCode <kazucuong@yahoo.com>
2018-07-31 18:33:50 +0000

    add more prototype of function

--- holberton.h
+++ holberton.h
@@ -1,13 +1,28 @@
 #ifndef _HOLBERTON_H
 #define _HOLBERTON_H
 
-/**
- * struct type - struct type
- * @type: format type
- * @f: function to print the type
- */
-
 int _printf(const char *format, ...);
 int _putchar(char c);
+int print_str(va_list ap);
+int print_per(void);
+int print_char(va_list ap);
+int print_int(va_list ap);
+int print_unsigned_int(va_list ap);
+int print_octal_num(va_list ap);
+int print_HEX_num(va_list ap);
+int print_hex_num(va_list ap);
+
+/**
+ * struct _printf_ - Store data of format and function pointer
+ * @format: format will be printed
+ * @f: function pointer
+ *
+ * Description: No
+ */
+typedef struct _printf_
+{
+	char *format;
+	int (*f)();
+} p;
 
 #endif /* _HOLBERTON_H */
